b067031572c88f4cbfbee8880a54a9a347564337
Psilon <pzixel@gmail.com>
2018-03-28 16:12:01 +0300

    Solidity

--- Solidity/Solidity.ProjectTemplate/Example.sol
+++ Solidity/Solidity.ProjectTemplate/Example.sol
@@ -1,25 +1,27 @@
-﻿contract mortal {
+﻿pragma solidity ^0.4.11;
+
+contract mortal {
     /* Define variable owner of the type address */
     address owner;
 
     /* This function is executed at initialization and sets the owner of the contract */
-    function mortal() { owner = msg.sender; }
+    function mortal() public {owner = msg.sender;}
 
     /* Function to recover the funds on the contract */
-    function kill() { if (msg.sender == owner) selfdestruct(owner); }
+    function kill() public {if (msg.sender == owner) selfdestruct(owner);}
 }
 
 contract greeter is mortal {
     /* Define variable greeting of the type string */
     string greeting;
-    
+
     /* This runs when the contract is executed */
     function greeter(string _greeting) public {
-    greeting = _greeting;
+        greeting = _greeting;
     }
 
     /* Main function */
-    function greet() constant returns (string) {
-    return greeting;
+    function greet() public constant returns (string) {
+        return greeting;
     }
 }
