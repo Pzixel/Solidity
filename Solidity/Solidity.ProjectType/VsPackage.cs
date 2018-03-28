c47b0e68227e1df859aa0a432cc163174faccef3
Psilon <pzixel@gmail.com>
2018-03-28 11:55:17 +0300

    Add project files.

---
+++ Solidity/Solidity.ProjectType/VsPackage.cs
@@ -0,0 +1,54 @@
+﻿/***************************************************************************
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+This code is licensed under the Visual Studio SDK license terms.
+THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+
+***************************************************************************/
+
+namespace Solidity
+{
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// This package is required if you want to define adds custom commands (ctmenu)
+    /// or localized resources for the strings that appear in the New Project and Open Project dialogs.
+    /// Creating project extensions or project types does not actually require a VSPackage.
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [Description("A custom project type based on CPS")]
+    [Guid(VsPackage.PackageGuid)]
+    public sealed class VsPackage : Package
+    {
+        /// <summary>
+        /// The GUID for this package.
+        /// </summary>
+        public const string PackageGuid = "348c2a37-df52-4d13-9a5c-45a6cb9b50fd";
+
+        /// <summary>
+        /// The GUID for this project type.  It is unique with the project file extension and
+        /// appears under the VS registry hive's Projects key.
+        /// </summary>
+        public const string ProjectTypeGuid = "9c9cc901-6370-4038-bff3-f730f76fac20";
+
+        /// <summary>
+        /// The file extension of this project type.  No preceding period.
+        /// </summary>
+        public const string ProjectExtension = "solidproj";
+
+        /// <summary>
+        /// The default namespace this project compiles with, so that manifest
+        /// resource names can be calculated for embedded resources.
+        /// </summary>
+        internal const string DefaultNamespace = "Solidity";
+    }
+}
