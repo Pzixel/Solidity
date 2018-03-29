ee7ad37dac71d00fb65cd61f668e0d10a935ed98
Psilon <pzixel@gmail.com>
2018-03-29 14:56:19 +0300

    Shine rebuild pipeline

--- Solidity/Solidity.ProjectTemplate/Example.sol
+++ Solidity/Solidity.ProjectTemplate/Example.sol
@@ -5,10 +5,16 @@ contract mortal {
     address owner;
 
     /* This function is executed at initialization and sets the owner of the contract */
-    function mortal() public {owner = msg.sender;}
+    function mortal() public {
+        owner = msg.sender;
+    }
 
     /* Function to recover the funds on the contract */
-    function kill() public {if (msg.sender == owner) selfdestruct(owner);}
+    function kill() public {
+        if (msg.sender == owner) {
+            selfdestruct(owner);
+        }
+    }
 }
 
 contract greeter is mortal {
